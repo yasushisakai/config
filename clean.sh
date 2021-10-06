34f994c849ce4f8f7293a65c5f42fe0582893f03
Yasushi Sakai <yasushi.accounts@fastmail.com>
2021-10-06 19:21:29 -0400

    adjusted clean and configure for mac
    clean and configure scripts will not delete/create symlinks

--- clean.sh
+++ clean.sh
@@ -17,29 +17,12 @@ force_rm() {
 	fi
 }
 
-force_rm .bash_history
-force_rm .bash_logout
-force_rm .bash_profile
-force_rm .bashrc
-force_rm_dir .doom.d
 force_rm .gitconfig
 force_rm .gitignore
 force_rm .mbsyncrc
-force_rm .notmuch-config
 force_rm .profile
-force_rm .xinitrc
-force_rm .Xmodmap
 
-force_rm_dir .config/alacritty
-force_rm_dir .config/ulauncher
-force_rm_dir .config/picom
-force_rm_dir .config/bspwm
 force_rm_dir .config/mutt
 force_rm_dir .config/msmtp
-force_rm_dir .config/polybar
-force_rm_dir .config/sxhkd
-force_rm_dir .config/checkmail
-force_rm_dir .config/systemd
 force_rm_dir .config/nvim
-
 
