aea0a1bbe6980d4eb32e70f2c446045a14acfc37
Yasushi Sakai <yasushi.accounts@fastmail.com>
2021-10-12 14:36:21 +0000

    doom for vm

--- doom.d/config.el
+++ doom.d/config.el
@@ -21,28 +21,11 @@
 ;; this is symlinked to ~/Dropbox/org
 (setq org-directory "~/org/")
 
+;; org-roam bibliography
+(setq bibtex-actions-bibliography '("~/Dropbox/bib/main.bib"))
+
 ;; org mode is org-roam
 (setq org-roam-directory org-directory)
 
 (add-hook 'org-mode-hook 'visual-line-mode)
 (add-hook 'org-mode-hook 'variable-pitch-mode)
-
-;; email
-(setq +notmuch-sync-backend 'mbsync)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
