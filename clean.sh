78a8e8a1f41536445635a6fcd2eb3583557ba87f
Yasushi Sakai <yasushi.accounts@fastmail.com>
2021-10-28 21:39:19 -0400

    revisiting mac

--- clean.sh
+++ clean.sh
@@ -1,6 +1,9 @@
 
 # removes directory if exists
 force_rm_dir() {
+	
+	echo "try rm -rf in $HOME/$1"
+
 	if [ -e $HOME/$1 ]
 	then
 		echo "removing dir: $HOME/$1"
@@ -10,6 +13,9 @@ force_rm_dir() {
 
 # removes file if exists
 force_rm() {
+	
+	echo "try rm-in $HOME/$1"
+
 	if [ -e $HOME/$1 ]
 	then
 		echo "removing: $HOME/$1"
@@ -23,6 +29,8 @@ force_rm .mbsyncrc
 force_rm Library/LaunchAgents/mbsync.plist
 force_rm .notmuch-config
 force_rm .profile
+force_rm .zshrc
+force_rm .zshenv
 
 force_rm_dir .config/mutt
 force_rm_dir .config/msmtp
--- configure.sh
+++ configure.sh
@@ -17,8 +17,11 @@ symlink mail/mbsync/mbsyncrc .mbsyncrc
 symlink mail/mbsync/mbsync.plist Library/LaunchAgents/mbsync.plist
 symlink mail/notmuch-config .notmuch-config
 symlink shell/profile .profile
+symlink shell/zshrc .zshrc
+symlink shell/zshenv .zshenv
 
 symlink mail/mutt .config/mutt
 symlink mail/msmtp .config/msmtp
 symlink nvim .config/nvim
+symlink doom.d .doom.d
 
